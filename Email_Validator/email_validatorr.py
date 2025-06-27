e99d47a928e2e58452720490422272ed7d16c6fc
Vivek Kadam <144525243+kadamvivek5708@users.noreply.github.com>
2025-06-27 19:41:17 +0530

    Update email_validatorr.py

--- Email_Validator/email_validatorr.py
+++ Email_Validator/email_validatorr.py
@@ -3,13 +3,13 @@ import email_validator as ev
 
 st.title('Email Validator')
 
-your_mail = st.text_input('Enter your email address ')
+email = st.text_input('Enter your email address ')
 verify = st.button('Verify')
 
 if verify:
     try:
-        valid = ev.validate_email(your_mail)
-        your_mail = valid.your_mail
+        valid = ev.validate_email(email)
+        email = valid.email
         st.success('Email is Valid')
 
     except ev.EmailNotValidError as e:
